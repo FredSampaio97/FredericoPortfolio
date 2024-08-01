7962899dec499b6f04c45866175ff1b96547c44b
unknown <fred.skp2@gmail.com>
2024-08-01 17:50:50 +0100

    New MyWork Feature

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -10,10 +10,11 @@ const MyWork = () => {
         <div className="mywork-title">
             <h1>My latest Work</h1>
             <img src={theme_pattern} alt="" />
+            
         </div>
         <div className="mywork-container">
             {mywork_data.map((work, index) => {
-                return <img key={index} src={work.w_img} alt='' />
+                return <a key={index} href={work.w_url}><img key={index} src={work.w_img} alt='' /></a>
             })}
         </div>
         <div >
