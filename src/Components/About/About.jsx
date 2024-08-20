d377d7cc2e52448574c57f6dee206b40da92e5b5
unknown <fred.skp2@gmail.com>
2024-08-20 16:23:02 +0100

    Mobile responsiveness

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import './About.css'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import profile_pic from '../../assets/profile_pic_fred_about.jpeg'
 
 
 const About = () => {
+
+    function adjustSkillBars(){
+        const skillBars = document.querySelectorAll('.skill-bar');
+        const screenWidth = window.innerWidth;
+
+        skillBars.forEach(skillbar => {
+            const originalWidth = skillbar.getAttribute('data-width');
+            if(screenWidth <= 600){
+                skillbar.style.width = `calc(${originalWidth} * 0.38)`;
+            }else{
+                skillbar.style.width = originalWidth;
+            }
+        })
+    }
+
+    useEffect(() => {
+        // Ensure that this runs after the DOM is fully loaded
+        window.addEventListener('load', adjustSkillBars);
+        window.addEventListener('resize', adjustSkillBars);
+    
+        return () => {
+          window.removeEventListener('load', adjustSkillBars);
+          window.removeEventListener('resize', adjustSkillBars);
+        };
+      }, []);
+
   return (
     <div id='about' className='about'>
         <div className="about-title">
@@ -21,12 +47,27 @@ const About = () => {
                     <p>Professionally, I'm organized, proactive, and work efficiently. I love teaching and helping others.</p>
                 </div>
                 <div className="about-skills">
-                    <div className="about-skill"><p>HTML & CSS</p> <hr style={{width: "80%"}} /></div>
-                    <div className="about-skill"><p>PHP & JavaScript</p> <hr style={{width: "60%"}} /></div>
-                    <div className="about-skill"><p>React Js & Node</p> <hr style={{width: "50%"}} /></div>
-                    <div className="about-skill"><p>PostgreSQL</p> <hr style={{width: "50%"}} /></div>
-                    <div className="about-skill"><p>Git e Github</p> <hr style={{width: "80%"}} /></div>
-                </div>
+                    <div className="about-skill">
+                        <p>HTML & CSS</p>
+                        <hr className="skill-bar" data-width="80%" />
+                    </div>
+                    <div className="about-skill">
+                        <p>PHP & JavaScript</p>
+                        <hr className="skill-bar" data-width="60%" />
+                    </div>
+                    <div className="about-skill">
+                        <p>React Js & Node</p>
+                        <hr className="skill-bar" data-width="50%" />
+                    </div>
+                    <div className="about-skill">
+                        <p>PostgreSQL</p>
+                        <hr className="skill-bar" data-width="50%" />
+                    </div>
+                    <div className="about-skill">
+                        <p>Git e Github</p>
+                        <hr className="skill-bar" data-width="80%" />
+                    </div>
+                    </div>
             </div>
         </div>
         <div className="about-achievements">
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,10 +10,19 @@ const Hero = () => {
         <img className='profile-pic' src={profile_pic} alt="" />
         <h1><span>I'm Frederico Sampaio</span>, FullStack Web Developer based in Lisbon.</h1>
         <p>At 26 years old, after working in accounting and administration for a year and a half, I found a new calling: to become a Full Stack Web Developer, dedicated to contributing to the success of web projects.</p>
-        <div className="hero-action">
-            <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <a className="hero-resume" href={resume}>My resume</a>
+        <div className="hero-desktop">
+          <div className="hero-action">
+              <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+              <a className="hero-resume" href={resume}>My resume</a>
+          </div>
         </div>
+        <div className="hero-mobile">
+          <div className="hero-action">
+              <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+              <a className="hero-resume" href={resume}>My resume</a>
+          </div>
+        </div>
+        
     </div>
   );
 };
