6254d812077dd3b52b04587d929563b7f18804a9
unknown <fred.skp2@gmail.com>
2024-10-14 15:27:39 +0100

    New Project

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,24 +4,32 @@ import project3_img from '../assets/phpwebsitedestaques.png'
 import project4_img from '../assets/phpwebsitefooter.png'
 import project5_img from '../assets/robofriends.png'
 import project6_img from '../assets/cryptoapp.png'
+import project7_img from '../assets/marmoresS.png'
 // import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
+    
     {
         w_no:1,
+        w_name:"Web design",
+        w_img:project7_img,
+        w_url: "https://marmoressampaio.onrender.com/"
+    },
+    {
+        w_no:2,
         w_name:"Web design",
         w_img:project2_img,
         w_url: "https://realestatewebsite-yip8.onrender.com/"
     },
     {
-        w_no:2,
+        w_no:3,
         w_name:"Web design",
         w_img:project6_img,
         w_url: "https://cryptoapp-2e54.onrender.com/"
     },
     
     {
-        w_no:3,
+        w_no:4,
         w_name:"Web design",
         w_img:project1_img,
         w_url: "https://facerecognitionapp-1.onrender.com"
